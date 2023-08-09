20b27f1eb04afde7b0c099d6b67fdb7d832d9b59
auseta <alseta97@gmail.com>
2023-08-09 13:38:58 -0300

    Creado directorio Services
    Se creo el archivo PizzaService.cs que crea un servicio de datos de pizza en memoria

---
+++ Services/PizzaService.cs
@@ -0,0 +1,43 @@
+using SetaPizza.Models;
+
+namespace SetaPizza.Services;
+
+public static class PizzaService
+{
+    static List<Pizza> Pizzas { get;}
+    static int nextId = 3;
+    static PizzaService()
+    {
+        Pizzas = new List<Pizza>
+        {
+            new Pizza { Id = 1, Nombre = "Bolognesa", LibreDeGluten = false},
+            new Pizza { Id = 2, Nombre = "Rucula Veggie", LibreDeGluten = true}
+        };
+    }
+
+    public static List<Pizza> GetAll() => Pizzas;
+
+    public static Pizza? Get(int id) => Pizzas.FirstOrDefault(p => p.Id == id);
+
+    public static void Add(Pizza pizza)
+    {
+        pizza.Id = nextId++;
+        Pizzas.Add(pizza);
+    }
+
+    public static void Delete(int id)
+    {
+        var pizza = Get(id);
+        if (pizza is null)
+            return;
+        Pizzas.Remove(pizza);
+    }
+
+    public static void Update(Pizza pizza)
+    {
+        var index = Pizzas.FindIndex(p => p.Id == pizza.Id);
+        if (index == 1) 
+            return;
+        Pizzas[index] = pizza;
+    }
+}
